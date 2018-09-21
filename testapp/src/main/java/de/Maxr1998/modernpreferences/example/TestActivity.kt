54fc3b1745ddb4de1246be26063edaade8e003c7
Maxr1998 <max.rumpf1998@gmail.com>
2018-09-21 12:48:10 +0200

    Update the TestApp

--- testapp/src/main/java/de/Maxr1998/modernpreferences/example/TestActivity.kt
+++ testapp/src/main/java/de/Maxr1998/modernpreferences/example/TestActivity.kt
@@ -1,13 +1,15 @@
 package de.Maxr1998.modernpreferences.example
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import de.Maxr1998.modernpreferences.PreferenceScreen
 import de.Maxr1998.modernpreferences.PreferencesAdapter
 import de.Maxr1998.modernpreferences.helpers.*
 
-class TestActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity(), PreferencesAdapter.OnScreenChangeListener {
 
     private val preferencesAdapter = PreferencesAdapter()
 
@@ -23,15 +25,17 @@ class TestActivity : AppCompatActivity() {
             pref("one") {
                 title = "Important"
                 description = "It actually is!"
+                iconRes = R.drawable.ic_important_24dp
             }
             pref("two") {
                 title = "Also important"
             }
             subScreen {
                 title = "Subscreen"
-
+                centerIcon = false
                 pref("three") {
                     title = "A not so important sub-preference"
+                    iconRes = R.drawable.ic_emoji_24dp
                 }
             }
             categoryHeader("header") {
@@ -46,10 +50,30 @@ class TestActivity : AppCompatActivity() {
                 iconRes = R.drawable.ic_emoji_24dp
             }
         })
+        preferencesAdapter.onScreenChangeListener = this
+    }
+
+    override fun onScreenChanged(screen: PreferenceScreen, subScreen: Boolean) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(subScreen)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onBackPressed() {
         if (!preferencesAdapter.goBack())
             super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        preferencesAdapter.onScreenChangeListener = null
+        super.onDestroy()
     }
 }
