0578a21fe522e65a30f2811a8ad83cefce704186
Maxr1998 <max.rumpf1998@gmail.com>
2020-04-11 19:39:07 +0200

    Convert root project to Kotlin build-scripts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        val kotlinVersion: String by project
+        classpath("com.android.tools.build:gradle:4.0.0-beta04")
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
