b1310c38c5fab2aa50806035d25ad3a752bb3598
Maxr1998 <max.rumpf1998@gmail.com>
2020-04-12 22:25:29 +0200

    Implement tick marks in ModernSeekBar itself to circumvent ProGuard issues from using reflection

--- library/src/main/java/de/Maxr1998/modernpreferences/views/ModernSeekBar.kt
+++ library/src/main/java/de/Maxr1998/modernpreferences/views/ModernSeekBar.kt
@@ -8,37 +8,14 @@ import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.view.ViewCompat
 import de.Maxr1998.modernpreferences.R
-import java.lang.reflect.Method
 
 class ModernSeekBar(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : AppCompatSeekBar(context, attrs, defStyleAttr) {
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.seekBarStyle)
     constructor(context: Context) : this(context, null)
 
-    var hasTickMarks
-        get() = getCompatTickMark() != null
+    private var tickMarkDrawable: Drawable? = null
         set(value) {
-            setCompatTickMark(if (value) {
-                context.getDrawable(R.drawable.map_seekbar_tick_mark)
-            } else null)
-        }
-
-    var default: Int? = null
-        set(value) {
-            require(value == null || value in 0..max) {
-                "Default must be in range 0 to max (is $value)"
-            }
-            field = value
-            if (value != null) {
-                if (defaultMarker == null)
-                    defaultMarker = context.getDrawable(R.drawable.map_seekbar_default_marker)
-            } else defaultMarker = null
-        }
-
-    private var defaultMarker: Drawable? = null
-        set(value) {
-            field?.apply {
-                callback = null
-            }
+            field?.callback = null
             field = value
 
             if (value != null) {
@@ -50,52 +27,99 @@ class ModernSeekBar(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
             invalidate()
         }
 
+    private var defaultMarkerDrawable: Drawable? = null
+        set(value) {
+            field?.callback = null
+            field = value
+
+            if (value != null) {
+                value.callback = this
+                DrawableCompat.setLayoutDirection(value, ViewCompat.getLayoutDirection(this))
+                if (value.isStateful) value.state = drawableState
+            }
+
+            invalidate()
+        }
+
+    var hasTickMarks
+        get() = tickMarkDrawable != null
+        set(value) {
+            if (value) {
+                if (tickMarkDrawable == null)
+                    tickMarkDrawable = context.getDrawable(R.drawable.map_seekbar_tick_mark)
+            } else tickMarkDrawable = null
+        }
+
+    var default: Int? = null
+        set(value) {
+            require(value == null || value in 0..max) {
+                "Default must be in range 0 to max (is $value)"
+            }
+            field = value
+            if (value != null) {
+                if (defaultMarkerDrawable == null)
+                    defaultMarkerDrawable = context.getDrawable(R.drawable.map_seekbar_default_marker)
+            } else defaultMarkerDrawable = null
+        }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (progress != default) drawDefaultMarker(canvas)
+        drawTickMarks(canvas)
+        drawDefaultMarker(canvas)
     }
 
     override fun drawableStateChanged() {
         super.drawableStateChanged()
-        val defaultMarker = defaultMarker
-        if (defaultMarker != null && defaultMarker.isStateful && defaultMarker.setState(drawableState)) {
-            invalidateDrawable(defaultMarker)
+        fun stateChanged(d: Drawable?) {
+            if (d != null && d.isStateful && d.setState(drawableState)) {
+                invalidateDrawable(d)
+            }
         }
+        stateChanged(tickMarkDrawable)
+        stateChanged(defaultMarkerDrawable)
     }
 
     override fun jumpDrawablesToCurrentState() {
         super.jumpDrawablesToCurrentState()
-        defaultMarker?.jumpToCurrentState()
+        tickMarkDrawable?.jumpToCurrentState()
+        defaultMarkerDrawable?.jumpToCurrentState()
+    }
+
+    private fun drawTickMarks(canvas: Canvas) {
+        tickMarkDrawable?.let { tickMark ->
+            val count = max
+            if (count > 1) {
+                val w = tickMark.intrinsicWidth
+                val h = tickMark.intrinsicHeight
+                val halfW = if (w >= 0) w / 2 else 1
+                val halfH = if (h >= 0) h / 2 else 1
+                tickMark.setBounds(-halfW, -halfH, halfW, halfH)
+                val spacing: Float = (width - paddingLeft - paddingRight) / count.toFloat()
+                val saveCount = canvas.save()
+                canvas.translate(paddingLeft.toFloat(), height / 2.toFloat())
+                for (i in 0..count) {
+                    tickMark.draw(canvas)
+                    canvas.translate(spacing, 0f)
+                }
+                canvas.restoreToCount(saveCount)
+            }
+        }
     }
 
     private fun drawDefaultMarker(canvas: Canvas) {
         val default = default ?: return
-        defaultMarker?.let {
-            val w: Int = it.intrinsicWidth
-            val h: Int = it.intrinsicHeight
+        if (default == progress) return
+        defaultMarkerDrawable?.let { defaultMarker ->
+            val w: Int = defaultMarker.intrinsicWidth
+            val h: Int = defaultMarker.intrinsicHeight
             val halfW = if (w >= 0) w / 2 else 1
             val halfH = if (h >= 0) h / 2 else 1
-            it.setBounds(-halfW, -halfH, halfW, halfH)
+            defaultMarker.setBounds(-halfW, -halfH, halfW, halfH)
             val saveCount = canvas.save()
             val spacing: Float = default * (width - paddingLeft - paddingRight) / max.toFloat()
             canvas.translate(paddingLeft.toFloat() + spacing, height / 2.toFloat())
-            it.draw(canvas)
+            defaultMarker.draw(canvas)
             canvas.restoreToCount(saveCount)
         }
-    }
-
-    private val helper: Any = AppCompatSeekBar::class.java.getDeclaredField("mAppCompatSeekBarHelper")
-            .apply { isAccessible = true }.get(this)!!
-    private val getTickMarkMethod: Method = helper::class.java.getDeclaredMethod("getTickMark")
-            .apply { isAccessible = true }
-    private val setTickMarkMethod: Method = helper::class.java.getDeclaredMethod("setTickMark", Drawable::class.java)
-            .apply { isAccessible = true }
-
-    private fun getCompatTickMark(): Drawable? {
-        return getTickMarkMethod.invoke(helper) as Drawable?
-    }
-
-    private fun setCompatTickMark(tickMark: Drawable?) {
-        setTickMarkMethod.invoke(helper, tickMark)
     }
 }
