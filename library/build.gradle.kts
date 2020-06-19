86a8f1b2e55fd56adbf458091a5957271256b93c
Maxr1998 <max.rumpf1998@gmail.com>
2020-06-19 15:45:46 +0200

    Release 0.5.7

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 // Versions
-val libraryVersion = "0.5.6"
+val libraryVersion = "0.5.7"
 val libraryName = "modernpreferences"
 val bintrayLibraryName = "ModernAndroidPreferences"
 
