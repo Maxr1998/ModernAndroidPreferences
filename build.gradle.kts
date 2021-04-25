cce2223e00071d6f4709d8daa33bbea8a7102803
Maxr1998 <max.rumpf1998@gmail.com>
2021-04-25 21:18:17 +0200

    Update android-junit5 plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         val kotlinVersion: String by project
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath(kotlin("gradle-plugin", kotlinVersion))
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
     }
 }
 
